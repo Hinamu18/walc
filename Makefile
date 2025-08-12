bd2fce3ed9f56a6df1b968fece6d884a5b67eb6b
Hinamu18 <hinamu17yt@gmail.com>
2025-08-12 16:39:48 +0300

    New Update

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
 all:
-	gcc main.c func.c -o walc -lm
+	gcc main.c func.c -o walc -I/usr/include/spng -lspng -lm
+
+Test:
+	gcc -O3 -flto -march=native -fopenmp  main.c func.c -o walc -I/usr/include/spng -lspng -lm 
+	
+	time ./walc ArchTan.png 
+
+op:
+	gcc -O2 -g -fno-omit-frame-pointer -march=native main.c func.c -o walc -I/usr/include/spng -lspng -lm
+
+WOW:
+	gcc -O3 -flto -march=native main.c func.c -o walc -I/usr/include/spng -lspng -lm
+
--- func.c
+++ func.c
@@ -2,28 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pixel_colors(int width, int height, int channels,unsigned char *data){
-  
+void pixel_colors(int width, int height, int channels, unsigned char *data) {
   FILE *hex_file = fopen("./data/hex.txt", "wb");
   int allP = width * height;
 
-  char *buffer = malloc(allP * ((channels == 3 ? 3*2 : 4*2)+2) );
+  // allocate enough memory to hold pixel data  
+  char *buffer = malloc(allP * channels * 3);
+  int index = 0;
   char *ptr = buffer;
- 
-    if (channels == 3) {
-      for (int i = 0; i < allP; i++) {
-      int index = i * channels;
-          ptr += sprintf(ptr, "#%02X%02X%02X\n", data[index], data[index + 1], data[index + 2]);
-      }
-    }else if (channels == 4) {
-      for (int i = 0; i < allP; i++) {
-      int index = i * channels;
-          ptr += sprintf(ptr, "#%02X%02X%02XFF\n", data[index], data[index + 1], data[index + 2]);
+  char hex[] = "0123456789ABCDEF";
+
+  for (int i = 0; i < width; i++) {
+    for (int k = 0; k < height; k++) {
+      index = (i * height + k) * channels;
+      if (channels == 3) {
+
+        *ptr++ = hex[data[i] >> 4];
+        *ptr++ = hex[data[i] & 0x0F];
+
+        *ptr++ = hex[data[i+1] >> 4];
+        *ptr++ = hex[data[i+1] & 0x0F];
+
+        *ptr++ = hex[data[i+2] >> 4];
+        *ptr++ = hex[data[i+2] & 0x0F];
+
+        *ptr++ = '\n';
+      } else if (channels == 4) {
+        *ptr++ = hex[data[i] >> 4];
+        *ptr++ = hex[data[i] & 0x0F];
+
+        *ptr++ = hex[data[i+1] >> 4];
+        *ptr++ = hex[data[i+1] & 0x0F];
+
+        *ptr++ = hex[data[i+2] >> 4];
+        *ptr++ = hex[data[i+2] & 0x0F];
+       
+        *ptr++ = hex[data[i+3] >> 4];
+        *ptr++ = hex[data[i+3] & 0x0F];
+        
+        *ptr++ = '\n';
+
       }
     }
+  }
   
+  /*
+  *ptr++ = data[index];
+  *ptr++ = data[index + 1];
+  *ptr++ = data[index + 2];
+  *ptr++ = 255;
+  */
 
   fwrite(buffer, 1, ptr - buffer, hex_file);
   free(buffer);
   fclose(hex_file);
 }
+
